d479c4777dd6765da0f84f24d787fb1cdb9ba7cd
billie115 <you@example.com>
2026-06-18 11:10:41 +0300

    commit 1

---
+++ app/Pages/Login.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="border border-secondary bg-dark text-light w-25 p-3 rounded mx-auto mt-5 shadow">
+        <form action="POST" id="formLogin">
+            <label htmlFor="Email" className='form-label'>Email:</label>
+            <input type="text" className='form-control' placeholder="Your Email goes here"/>
+            <label htmlFor="Password" className='form-label'>Password:</label>
+            <input type="password" className='form-control' placeholder="Your Password goes here"/>
+            <button type="submit" className='btn btn-primary mt-2'>LogIn</button>
+        </form>
+    </div>
+  );
+}
+export default App; 
