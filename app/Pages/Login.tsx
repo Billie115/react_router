46429854a6af8e33fca503a69fba91347082eba3
billie115 <you@example.com>
2026-06-18 15:05:43 +0300

    commit 4

--- app/Pages/Login.tsx
+++ app/Pages/Login.tsx
@@ -1,18 +1,17 @@
 
 function App() {
   return (
-    <div className=".bg-light-subtle">
-        <div className="border border-secondary bg-dark text-light w-25 p-3 rounded mx-auto mt-5 shadow">
+    <body className="bg-dark text-white">
+        <div className="border border-secondary bg-primary text-white w-25 p-3 rounded mx-auto mt-5 shadow">
             <form action="POST" id="formLogin">
                 <label htmlFor="Email" className='form-label'>Email:</label>
                 <input type="text" className='form-control' placeholder="Your Email goes here"/>
                 <label htmlFor="Password" className='form-label'>Password:</label>
                 <input type="password" className='form-control' placeholder="Your Password goes here"/>
-                <button type="submit" className='btn btn-primary mt-2'>LogIn</button>
+                <button type="submit" className='btn btn-light mt-2'>LogIn</button>
             </form>
         </div>
-    </div>
-    
+    </body>
   );
 }
 export default App; 
